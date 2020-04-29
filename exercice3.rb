5d82f9c082c0019c26caa92012aaff2089a92cf1
Robotgeek <jacques.cai1999@gmail.com>
2020-04-29 11:47:01 +0200

    Exercice 3

---
+++ exercice3.rb
@@ -0,0 +1,18 @@
+array = []
+
+
+for i in 0..4
+  puts "Saisissez la valeur #{i}"
+  array += [gets.chomp.to_i]
+end
+
+maximun = 0
+for i in 1..(array.length-1)
+  if array[i] > array[maximun]
+    maximun = i
+  end
+end
+
+puts "Le plus grand élement est : #{array[maximun]}"
+
+puts "La moyenne est " + (array.reduce{|a,b| a + b}/array.length).to_s
